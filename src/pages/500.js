685cf54a67afb7c48fba6783c61151f10c5ec08c
Karl F. Sellergren <68159964+sakerhetspolisen@users.noreply.github.com>
2024-05-17 03:52:12 +0200

    Updated 500-page (#204)
    * Updated 500.js

--- src/pages/500.js
+++ src/pages/500.js
@@ -1,7 +1,8 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
 import Head from "next/head";
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import errorBgd from "@/assets/errorBgd.jpg";
 
 export default function NotFound() {
@@ -22,17 +23,48 @@ export default function NotFound() {
           fill
           style={{ objectFit: "cover" }}
         />
-        <Box textAlign="center" zIndex={1} color="white">
-          <Text fontWeight="500" mb={8}>
-            500 - Det har skett ett fel på vår sida
-          </Text>
-          <Heading>Det där skulle ju inte ha skett.</Heading>
-          <Text fontSize="1.2em" mt={2}>
-            Våra system har fått information om händelsen. Nu är det bara upp
-            till vädergudarna.
-          </Text>
+        <Box textAlign="center" zIndex={1} color="white" px={4}>
+          <Heading mb={8}>Då var det slut på det roliga.</Heading>
+          <Box w="100%" maxW="800px">
+            <Text fontSize="1.2em" mt={2}>
+              UV-Kollen är ett hobbyprojekt som drivs av mig, Karl Sellergren,
+              tack vare donationer från dess användare. I dagsläget kan
+              UV-Kollen hämta data för som mest 10 000 användare per dag och
+              idag har antalet användare överstigit det.
+            </Text>
+            <Text fontSize="1.2em" fontWeight={700} mt={6} mb={4}>
+              Hjälp till att hålla igång projektet genom att donera en liten
+              slant!
+            </Text>
+            <Link
+              href="https://www.paypal.com/paypalme/sellergreen"
+              rel="noindex nofollow"
+              target="_blank"
+            >
+              <Button leftIcon={<HeartIcon />} colorScheme="yellow">
+                Sponsra
+              </Button>
+            </Link>
+          </Box>
         </Box>
       </Box>
     </main>
+  );
+}
+
+function HeartIcon() {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 512 512"
+      focusable="false"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z" />
+    </svg>
   );
 }
